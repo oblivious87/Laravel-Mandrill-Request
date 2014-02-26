9c1b7f5de3bd365ad39c00053d320be326df4baf
Jesse Stark <jstark@gmail.com>
2014-02-25 23:09:03 -0600

    Local Version

--- src/oblivious87/LaravelMandrillRequestLocal/MandrillRequestLocal.php
+++ src/oblivious87/LaravelMandrillRequestLocal/MandrillRequestLocal.php
@@ -1,10 +1,11 @@
-<?php namespace Doxxon\LaravelMandrillRequest;
+<?php namespace oblivious87\LaravelMandrillRequestLocal;
 
 /**
- * An L4 package wrapper of Michael Teeuw's Laravel 3 mandrill API wrapper
+ * An L4 package wrapper of Michael Teeuw's Laravel 3 mandrill API wrapper written by Doxxon 
+ * I added Local environment support for the SSL connection.
  *
- * @link 		https://github.com/doxxon/laravel-mandrill
- * @author     	Doxxon <doxxon.co@gmail.com>
+ * @link 		https://github.com/oblivious87/laravel-mandrill
+ * @author     	oblivious87 <jstark@gmail.com>
  * @license    	MIT License
  */
 
@@ -21,23 +22,34 @@ class MandrillRequest
 		
 		// build payload
 		$arguments['key'] = $api_key;
-		
+
 		// setup curl request
 		$ch = curl_init();
+		
+		if (\App::environment('local'))
+		{
+		    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // this has to be at the end of the array?
+		}
+		else
+		{
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // this has to be at the end of the array?
+		}
+
 		curl_setopt($ch, CURLOPT_URL, $endpoint);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		curl_setopt($ch, CURLOPT_POST, true);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arguments));
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // this has to be at the end of the array?
+		
 		$response = curl_exec($ch);
 
 		// catch errors
 		if (curl_errno($ch))
 		{
-			#$errors = curl_error($ch);
+			//$errors = curl_error($ch);
 			curl_close($ch);
 			
 			// return false
